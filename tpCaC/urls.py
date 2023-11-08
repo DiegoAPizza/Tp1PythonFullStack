3acb6607187e8061c7c7cf96be32e8a9b7ea10b3
Diego A.Pizza <79482954+DiegoAPizza@users.noreply.github.com>
2023-11-08 20:00:55 -0300

    .
    .

--- tpCaC/urls.py
+++ tpCaC/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from prueba.views import hello
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',hello)
 ]
