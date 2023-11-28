3749ad29a91a0d68fefe0e366828d3c666ce23bb
Diego A.Pizza <79482954+DiegoAPizza@users.noreply.github.com>
2023-11-28 19:03:46 -0300

    cosas
    Cosas magiacas

--- prueba/models.py
+++ prueba/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from uuid import uuid4
 # Create your models here.
+class Tarea(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid4,editable=False)
+    nombre = models.CharField(max_length=200)
+    class Meta:
+        verbose_name='Tarea'
+        verbose_name_plural='Tareas'
+        db_table='Tarea'
+    
--- tpCaC/settings.py
+++ tpCaC/settings.py
@@ -78,7 +78,10 @@ WSGI_APPLICATION = 'tpCaC.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'tpcacpython.sqlite3',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'tpcacpython',
     }
 }
 
