eb32b7c74efc82e35a059c08add2a099429a1977
Diego A.Pizza <79482954+DiegoAPizza@users.noreply.github.com>
2023-12-01 00:20:09 -0300

    -x
    x

--- prueba/urls.py
+++ prueba/urls.py
@@ -1,16 +1,13 @@
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
-from prueba import views
+from . import views
 
    
 
 urlpatterns = [
     path('',views.inicio,name="index"),
     path('login2/',views.login_prueba,name="login2"),
-    path('login/',views.login_prueba,name="login"),
-    
-    path('',views.inicio,name="index"),
-    path('login/',views.login,name="login") ,
+    path('login/',views.login,name="login"),
     
 ]
 urlpatterns += staticfiles_urlpatterns()
--- tpCaC/settings.py
+++ tpCaC/settings.py
@@ -125,11 +125,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR ,'prueba/static'),)
-
-STATICFILES_DIRS =['C:/Users/facus/OneDrive/Documents/1 PYTHON FULL STACK/Tp1PythonFullStack/prueba/static',]
-
-STATIC_ROOT = BASE_DIR /'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
