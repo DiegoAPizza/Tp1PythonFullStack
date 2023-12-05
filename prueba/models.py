0f048c7c543ef232ae1890f305d7f0d52ce75192
Diego A.Pizza <79482954+DiegoAPizza@users.noreply.github.com>
2023-12-05 17:41:22 -0300

    mejoras en los modelos falta la integracion

--- prueba/models.py
+++ prueba/models.py
@@ -1,19 +1,30 @@
 from django.db import models
 from uuid import uuid4
 # Create your models here.
-class Cliente(models.Model):
-    id=models.AutoField(primary_key=True)
-    nombre=models.CharField(max_length=45)
-    apellido=models.CharField(max_length=45)
-    email=models.CharField(max_length=45)
-    idServicio=models.ForeignKey()
-    idProducto=models.ForeignKey()
-    idMetodoDePago=models.ForeignKey(to=Producto)
-    idMetodoDeCobro=models.ForeignKey()
+
+
+
+class Servicio(models.Model):
+    idServicio=models.AutoField(primary_key=True)
+    # Definir el modelo para Servicio
+class MetodoDePago(models.Model):
+    idCliente=models.AutoField(primary_key=True)
+class MetodoDeCobro(models.Model):
+   idCliente=models.AutoField(primary_key=True)
 class Producto(models.Model):
     idProducto=models.AutoField(primary_key=True)
     nombre=models.CharField(max_length=45)
     cantidad=models.IntegerField(max_length=200)
     imagen=models.ImageField(upload_to='imagenes/',null=True,blank=True)
+class Cliente(models.Model):
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=45)
+    apellido=models.CharField(max_length=45)
+    email=models.CharField(max_length=45)
+    idServicio=models.ForeignKey(Servicio, on_delete=models.CASCADE)
+    idProducto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    idMetodoDePago=models.ForeignKey(MetodoDePago,on_delete=models.CASCADE)
+    idMetodoDeCobro=models.ForeignKey(MetodoDeCobro,on_delete=models.CASCADE)
+
     
     
