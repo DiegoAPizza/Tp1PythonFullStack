838ba9eac2732f72c9ac7efcbd5b33249e837e8e
Diego A.Pizza <79482954+DiegoAPizza@users.noreply.github.com>
2023-12-07 20:56:25 -0300

    v
    v

--- prueba/admin.py
+++ prueba/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Servicio, MetodoDeCobro, MetodoDePago, Categoria, Producto, Cliente
+
+#Register your models here.
+admin.site.register(Servicio)
+admin.site.register(MetodoDeCobro)
+admin.site.register(MetodoDePago)
+admin.site.register(Categoria)
+admin.site.register(Producto)
+admin.site.register(Cliente)
--- prueba/models.py
+++ prueba/models.py
@@ -1,41 +1,40 @@
 from django.db import models
 from uuid import uuid4
-# Create your models here.
 
 class Servicio(models.Model):
-    idServicio=models.AutoField(primary_key=True)
-    nombre=models.CharField(max_length=45)
-    precio=models.models.IntegerField(max_length=12)
+    idServicio = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=45)
+    precio = models.IntegerField()
 
-    # Definir el modelo para Servicio
 class MetodoDePago(models.Model):
-    idCliente=models.AutoField(primary_key=True)
-    
+    idPago = models.AutoField(primary_key=True)
+    tipoMetPag = models.CharField(max_length=30, default=' ')
+
 class MetodoDeCobro(models.Model):
-   idCliente=models.ForeignKey(Cliente, on_delete=models.CASCADE)
-   idMetod=models.AutoField(primary_key=True)
+    idMetod = models.AutoField(primary_key=True)
+    tipoMet = models.CharField(max_length=30, default=1) 
+    idServicio = models.ForeignKey(Servicio, on_delete=models.CASCADE, default=1)
 
-class Categorias(models.Model):
-    id=models.AutoField(primary_key=True)
-    nombreCategaria=models.CharField(max_leght=100)
-    idServicio=models.ForeignKey(Servicio,on_delete=models.CASCADE)
 
+
+class Categoria(models.Model):
+    idCat = models.AutoField(primary_key=True)
+    nombreCategoria = models.CharField(max_length=100)
+    idServicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
 
 class Producto(models.Model):
-    idProducto=models.AutoField(primary_key=True)
-    nombre=models.CharField(max_length=45)
-    cantidad=models.IntegerField(max_length=200)
-    imagen=models.ImageField(upload_to='imagenes/',null=True,blank=True)
-    descriptcion=models.TextField(max_length=299)
-class Cliente(models.Model):
-    id=models.AutoField(primary_key=True)
-    nombre=models.CharField(max_length=45)
-    apellido=models.CharField(max_length=45)
-    email=models.CharField(max_length=45)
-    idServicio=models.ForeignKey(Servicio, on_delete=models.CASCADE)
-    idProducto = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    idMetodoDePago=models.ForeignKey(MetodoDePago,on_delete=models.CASCADE)
-    idMetodoDeCobro=models.ForeignKey(MetodoDeCobro,on_delete=models.CASCADE)
+    idProducto = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=45)
+    cantidad = models.IntegerField()
+    imagen = models.ImageField(upload_to='imagenes/', null=True, blank=True)
+    descripcion = models.TextField(max_length=299)
 
-    
-    
+class Cliente(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=45)
+    apellido = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    idServicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
+    idProducto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    idMetodoDePago = models.ForeignKey(MetodoDePago, on_delete=models.CASCADE)
+    idMetodoDeCobro = models.ForeignKey(MetodoDeCobro, on_delete=models.CASCADE)
